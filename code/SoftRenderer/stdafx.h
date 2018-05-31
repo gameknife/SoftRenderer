f781ab6a384082dfb1d69b183acca36017804bf1
gameKnife <kaimingyi@163.com>
2018-05-31 20:36:26 +0800

    fix compile issue on vs2017

--- code/SoftRenderer/stdafx.h
+++ code/SoftRenderer/stdafx.h
@@ -19,8 +19,8 @@
 
 #define _ATL_CSTRING_EXPLICIT_CONSTRUCTORS      // some CString constructors will be explicit
 
-#include <atlbase.h>
-#include <atlstr.h>
+//#include <atlbase.h>
+//#include <atlstr.h>
 
 // TODO: reference additional headers your program requires here
 #include "prerequisite.h"
