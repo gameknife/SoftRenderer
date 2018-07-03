426c37f68b2f5c20a7445d65cd84404939b0fc96
gameKnife <kaimingyi@163.com>
2018-07-03 19:47:19 +0800

    bitmap use self

--- code/SoftRenderer/SrBitmap.cpp
+++ code/SoftRenderer/SrBitmap.cpp
@@ -15,6 +15,41 @@
 
 #include "mmgr.h"
 
+
+
+const int MIN_RGB = 0;
+const int MAX_RGB = 255;
+const int BMP_MAGIC_ID = 2;
+
+struct bmpfile_magic
+{
+	unsigned char magic[BMP_MAGIC_ID];
+};
+
+struct bmpfile_header
+{
+	uint32_t file_size;
+	uint16_t creator1;
+	uint16_t creator2;
+	uint32_t bmp_offset;
+};
+
+struct bmpfile_dib_info
+{
+	uint32_t header_size;
+	int32_t width;
+	int32_t height;
+	uint16_t num_planes;
+	uint16_t bits_per_pixel;
+	uint32_t compression;
+	uint32_t bmp_byte_size;
+	int32_t hres;
+	int32_t vres;
+	uint32_t num_colors;
+	uint32_t num_important_colors;
+};
+
+
 SrBitmap::SrBitmap(const char* filename):SrTexture(filename)
 {
 	std::string realpath(filename);
@@ -29,25 +64,26 @@ SrBitmap::SrBitmap(const char* filename):SrTexture(filename)
 		const char* start = bitmapfile.Data();
 
 		// 获得文件头
-		tagBITMAPFILEHEADER* header = (tagBITMAPFILEHEADER*)start;
-		tagBITMAPINFO* info = (tagBITMAPINFO*)(start + sizeof(tagBITMAPFILEHEADER));
+		bmpfile_magic* magic = (bmpfile_magic*)start;
+		bmpfile_header* header = (bmpfile_header*)(start + sizeof(bmpfile_magic));
+		bmpfile_dib_info* info = (bmpfile_dib_info*)(start + +sizeof(bmpfile_magic) + sizeof(bmpfile_header));
 
 		// 初始化debug数据
 		GtLog("BMP file openup[%s] width: %d | height: %d | bpp: %d | offset: %d | size: %d kb", getName(),
-			info->bmiHeader.biWidth,
-			info->bmiHeader.biHeight,
-			info->bmiHeader.biBitCount,
-			header->bfOffBits,
-			header->bfSize / 1024 );
+			info->width,
+			info->height,
+			info->bits_per_pixel,
+			header->bmp_offset,
+			header->file_size / 1024 );
 
 		// 基本数据读取
-		m_width = info->bmiHeader.biWidth;
-		m_height = abs(info->bmiHeader.biHeight);
-		m_bpp = info->bmiHeader.biBitCount / 8;
-		m_pitch = -m_width * m_bpp * m_height / info->bmiHeader.biHeight;
+		m_width = info->width;
+		m_height = abs(info->height);
+		m_bpp = info->bits_per_pixel / 8;
+		m_pitch = -m_width * m_bpp * m_height / info->height;
 		// copy file
 		m_rawData = (uint8*)_mm_malloc_custom( m_bpp * m_width * m_height, 16);
-		uint8* colorstart = (uint8*)(start + header->bfOffBits);
+		uint8* colorstart = (uint8*)(start + header->bmp_offset);
 
 		memcpy(m_rawData, colorstart, m_bpp * m_width * m_height);
 
